9c10191ed6fd780db38f113d96a6102dfd9edf5d
samofoke <samofoke@student.wethinkcode.co.za>
2020-04-15 21:11:44 +0200

    build3

--- learn1/App.js
+++ learn1/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { StyleSheet, View} from 'react-native';
 import Header from './component/header/header.component';
+import StartGame from './Screens/start-game/start-game.component';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
+      <StartGame />
     </View>
   );
 }
--- learn1/Screens/start-game/start-game.component.js
+++ learn1/Screens/start-game/start-game.component.js
@@ -1,2 +1,49 @@
 import React from 'react';
 
+import {StyleSheet, View, Text, TextInput, Button} from 'react-native';
+
+const startGame = props => {
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.title}>Start a new Game</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a Number</Text>
+                <TextInput />
+                <View style={styles.containerButton}>
+                    <Button title="reset" onPress={() => {
+
+                    }} />
+                    <Button title="confirm" onPress={() => {
+
+                    }} />
+                </View>
+            </View>
+        </View>
+    )
+};
+
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    containerButton: {
+       flexDirection: 'row',
+       width: '100%',
+       justifyContent: 'space-between',
+       paddingHorizontal: 15
+    }
+});
+
+export default startGame;
